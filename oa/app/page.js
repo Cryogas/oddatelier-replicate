3502fc706b48d36a10defcae2c3218577374627c
Cryogas <136941894+Cryogas@users.noreply.github.com>
2023-12-29 22:09:23 +0800

    Finally no error message

---
+++ oa/app/page.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-row justify-between p-24">
+      <h1 className="text-red-300">Hello</h1>
+      <h1>Hel</h1>
+    </main>
+  )
+}
