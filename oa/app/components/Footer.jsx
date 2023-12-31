dd9992f9b4bd693575ec2bd9402f8e70d100579d
Cryogas <136941894+Cryogas@users.noreply.github.com>
2023-12-31 23:19:27 +0800

    Footer Adjust OK

--- oa/app/components/Footer.jsx
+++ oa/app/components/Footer.jsx
@@ -5,11 +5,11 @@ import React from 'react'
 
 const Footer = () => {
   const line1 = "© ODDATELIER. ALL RIGHTS RESERVED."
-  const line2 = "ALL OF ODDATELIER’S CONTENT IS COPYRIGHTED."
+  const line2 = "ALL OF ODDATELIER\’S CONTENT IS COPYRIGHTED."
   
   return (
     <footer className='fixed bottom-0 pb-4 w-full flex justify-center border border-white'>
-      <div className='lg:flex lg:flex-col items-cen'>
+      <div className='lg:flex lg:flex-col'>
         <h3 className=' border border-red-300 font-sweetsanspro text-[10px] xl:text-[12px] whitespace-pre-line lg:whitespace-normal text-center'>
           {line1}
           <span className='block xl:inline'> {line2}</span>
