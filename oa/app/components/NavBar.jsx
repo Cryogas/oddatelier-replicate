b1661f0b450f598a87bfb719d2704986ded9fdc2
Cryogas <136941894+Cryogas@users.noreply.github.com>
2024-01-02 21:03:17 +0800

    added 2 bars

--- oa/app/components/NavBar.jsx
+++ oa/app/components/NavBar.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
-import Head from 'next/head';
 
 const NavBar = () => {
+
+
   return (
     <React.Fragment>
-    <Head>
-      <title>Hello</title>
-    </Head>
       <main className="flex flex-row justify-between pb-10 sticky top-0 border border-purple-500">
         <a href='' className="text-red-300 ml-4 h-20 w-20 text-center">
           <Image 
@@ -21,10 +19,12 @@ const NavBar = () => {
         </a>
 
         <div className='flex flex-col border border-purple-300 font-sweetsanspro text-right'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
-            className="invert-[.25] w-8 h-8 ml-auto mt-4 mr-4 transistion duration-50 hover:invert-0">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
-            </svg>
+
+        <div className='flex flex-col py-4 ml-auto mr-3'>
+          <div className='bg-slate-100 h-[3px] w-9 my-1'></div>
+          <div className='bg-slate-100 h-[3px] w-9 my-1'></div>
+        </div>
+          
 
           <a href='#' className="mr-2 ">ODDATELIER</a>
           <a href='#' className="mr-2 ">ARTIST</a>
